793d26ac8e598956a619992451c3aac3931c8ad1
Anderson Conceição <andersonconceiicao@gmail.com>
2022-11-27 19:48:55 +1100

    feat: added project layers

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NotfoundPageComponent } from './notfound-page/notfound-page.component';
+
+
+
+@NgModule({
+  declarations: [
+    NotfoundPageComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
