8511a756bb220723c9a310720782c662320fddbd
Anderson Conceição <andersonconceiicao@gmail.com>
2022-11-29 12:44:56 +1100

    feat: added routers and lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,41 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { NotfoundPageComponent } from "./shared/notfound-page/notfound-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "home",
+    loadChildren: () =>
+      import("./pages/home/home.module").then((x) => x.HomeModule),
+  },
+  {
+    path: "about",
+    loadChildren: () =>
+      import("./pages/about/about.module").then((m) => m.AboutModule),
+  },
+  {
+    path: "contact",
+    loadChildren: () =>
+      import("./pages/contact/contact.module").then((m) => m.ContactModule),
+  },
+  {
+    path: "project",
+    loadChildren: () =>
+      import("./pages/projects/projects.module").then((m) => m.ProjectsModule),
+  },
+  {
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  {
+    path: "**",
+    component: NotfoundPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotfoundPageComponent } from './notfound-page/notfound-page.component';
+import { HeaderComponent } from './header/header.component';
 
 
 
 @NgModule({
   declarations: [
-    NotfoundPageComponent
+    NotfoundPageComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
