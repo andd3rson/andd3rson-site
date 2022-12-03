82bb867393fdb8fd788558edfe7d32831fc0ffba
Anderson Conceição <andersonconceiicao@gmail.com>
2022-12-03 20:12:48 +1100

    feat: created form to send message for me. And prepared layout to insert my current location.

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
-
-  constructor() { }
+  form!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: ["", [Validators.email, Validators.required]],
+      message: ["", [Validators.required, Validators.maxLength(200)]],
+    });
   }
 
+  handlerEmail(){
+    console.log(this.form.value)
+  }
 }
