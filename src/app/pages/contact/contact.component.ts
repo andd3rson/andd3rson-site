f052b2097238a1ddafaf2cbd3f6b59c82f2438c8
Anderson Conceição <andersonconceiicao@gmail.com>
2022-12-03 23:17:18 +1100

    feat[contact]: added cards about my informations

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,6 +1,42 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
+const CONTACTS = [
+  {
+    title: "E-mail me",
+    description: "andersonconceiicao@outlook.com",
+    classNAme: ""
+  },
+  {
+    title: "Sociais Medias",
+    description: null,
+    urls: [
+      {
+        path: "https://www.linkedin.com/in/anderson-d-3280711b4/",
+        icon: "linkedin"
+      },
+      {
+        path: "https://github.com/andd3rson",
+        icon: "github",
+      },
+      {
+        path: "https://www.instagram.com/andd3rson/",
+        icon: "instagram",
+      },
+    ],
+    classNAme: ""
+  },
+  {
+    title: "Call me",
+    description: "+61 0404 069 145",
+    classNAme: "fa-sharp fa-solid fa-location-dot"
+  },
+  {
+    title: "My address",
+    description: "888 Collins St. Docklands",
+  },
+];
+
 @Component({
   selector: "app-contact",
   templateUrl: "./contact.component.html",
@@ -8,6 +44,9 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class ContactComponent implements OnInit {
   form!: FormGroup;
+
+  contactsCards = CONTACTS;
+
   constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
@@ -17,7 +56,7 @@ export class ContactComponent implements OnInit {
     });
   }
 
-  handlerEmail(){
-    console.log(this.form.value)
+  handlerEmail() {
+    console.log(this.form.value);
   }
 }
