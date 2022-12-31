cd2d4a2f25e77818fcc41735e0235c42eb6c1e91
Anderson Conceição <andersonconceiicao@gmail.com>
2022-12-31 13:58:25 +1100

    fix[contact]: changed color pallet

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
 import { ProjectsService } from "./projects.service";
 import { Project } from "./project.model";
 
